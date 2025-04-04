2d72c26ad06e61c3234df84218162e346f50d4f2
AnjushreeJ <anjushreej5@gmail.com>
2025-04-04 17:26:07 +0530

    Add files via upload

---
+++ src/components/Course.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text-2xl  md:text-4xl">
+            We're delighted to have you{" "}
+            <span className="text-pink-500"> Here! :)</span>
+          </h1>
+          <p className="mt-12">
+          Discover a new era of online auctions with our cutting-edge
+                platform designed to bring buyers and sellers together in a
+                seamless, secure, and engaging environment. Whether you are
+                looking to find great deals or sell unique items, our system
+                provides real-time bidding, transparent transactions, and a wide
+                array of categories to explore. Join our community today and
+                experience the excitement of winning big in the world of online
+                auctions!
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
